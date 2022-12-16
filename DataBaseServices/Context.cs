a4065c7b2619c70934230529b8d828a6fc661d52
Dmitriy Krekshin <dima.krejshin@gmail.com>
2022-12-16 16:08:49 +0300

    create context

---
+++ DataBaseServices/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseServices;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options) { }
+
+    public DbSet<AmountHistoryEntity> AmountHistory { get; set; } = null!;
+    
+    public DbSet<CategoryEntity> Categories { get; set; } = null!;
+    
+    public DbSet<CategorySumEntity> CategorySums { get; set; } = null!;
+    
+    public DbSet<UserEntity> Users { get; set; } = null!;
+}
