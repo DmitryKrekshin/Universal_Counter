292559c631fd2b9116d076a1e0674b3936912af8
Dmitriy Krekshin <dima.krejshin@gmail.com>
2022-12-16 16:12:03 +0300

    create user service

---
+++ DataBaseServices/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBaseServices;
+
+public interface IUserService
+{
+    public void AddUser(UserEntity userEntity);
+
+    public void UpdateUser(UserEntity userEntity);
+
+    public void DeleteUser(int userId);
+
+    public IEnumerable<UserEntity> GetUsers(Func<UserEntity, bool> predicate);
+}
---
+++ DataBaseServices/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿namespace DataBaseServices;
+
+public class UserService : IUserService
+{
+    private readonly Context _context;
+
+    public UserService(Context context) => _context = context;
+
+    public void AddUser(UserEntity userEntity)
+    {
+        _context.Users.AddAsync(userEntity);
+        _context.SaveChangesAsync();
+    }
+
+    public void UpdateUser(UserEntity userEntity)
+    {
+        var categoryToUpdate = _context.Users.Find(userEntity.Id);
+
+        if (categoryToUpdate == null)
+        {
+            throw new NullReferenceException("User doesn't exists");
+        }
+
+        categoryToUpdate = userEntity;
+        _context.SaveChangesAsync();
+    }
+    
+    public void DeleteUser(int userId)
+    {
+        var userToDelete = _context.Users.Find(userId);
+
+        if (userToDelete == null)
+        {
+            throw new NullReferenceException("User doesn't exists");
+        }
+
+        _context.Users.Remove(userToDelete);
+        _context.SaveChangesAsync();
+    }
+
+    public IEnumerable<UserEntity> GetUsers(Func<UserEntity, bool> predicate)
+    {
+        return _context.Users.Where(predicate).ToList();
+    }
+}
