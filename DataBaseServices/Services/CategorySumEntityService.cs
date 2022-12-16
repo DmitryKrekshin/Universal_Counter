ffa2f1d2c799903859b2f63f59220fd9cd1d447f
Dmitriy Krekshin <dima.krejshin@gmail.com>
2022-12-16 16:30:54 +0300

    create category sum service

---
+++ DataBaseServices/Services/CategorySumEntityService.cs
@@ -0,0 +1,48 @@
+﻿namespace DataBaseServices;
+
+public class CategorySumEntityService : ICategorySumEntity
+{
+    private readonly Context _context;
+
+    public CategorySumEntityService(Context context)
+    {
+        _context = context;
+    }
+    
+    public void Add(CategorySumEntity categorySumEntity)
+    {
+        _context.CategorySums.AddAsync(categorySumEntity);
+        _context.SaveChangesAsync();
+    }
+
+    public void Update(CategorySumEntity categorySumEntity)
+    {
+        var categorySumToUpdate = _context.CategorySums.Find(categorySumEntity.CategoryEntityId);
+
+        if (categorySumToUpdate == null)
+        {
+            throw new NullReferenceException("CategorySum doesn't exists");
+        }
+
+        _context.CategorySums.Update(categorySumEntity);
+        _context.SaveChangesAsync();
+    }
+
+    public void Delete(int categorySumId)
+    {
+        var categorySumToDelete = _context.CategorySums.Find(categorySumId);
+        
+        if (categorySumToDelete == null)
+        {
+            throw new NullReferenceException("CategorySum doesn't exists");
+        }
+
+        _context.CategorySums.Remove(categorySumToDelete);
+        _context.SaveChangesAsync();
+    }
+
+    public IEnumerable<CategorySumEntity> Get(Func<CategorySumEntity, bool> predicate)
+    {
+        return _context.CategorySums.Where(predicate).ToList();
+    }
+}
