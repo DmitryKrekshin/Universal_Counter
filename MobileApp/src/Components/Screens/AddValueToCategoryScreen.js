03960c7fcfa4076814468dfe2134ae349a321ac2
Dmitriy Krekshin <dima.krejshin@gmail.com>
2023-03-05 18:57:08 +0300

    #8 install react-native-virtual-keyboard package + layout

--- MobileApp/src/Components/Screens/AddValueToCategoryScreen.js
+++ MobileApp/src/Components/Screens/AddValueToCategoryScreen.js
@@ -1,32 +1,68 @@
 import React, {useState} from "react";
-import {Animated, Pressable, StyleSheet, Text, useWindowDimensions, View} from "react-native";
+import {Animated, Modal, Pressable, StyleSheet, useWindowDimensions, View} from "react-native";
 import {useCardAnimation} from "@react-navigation/stack";
+import {Button, Input, Text} from "@rneui/themed";
+import VirtualKeyboard from "react-native-virtual-keyboard";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const AddValueToCategoryScreen = ({navigation, route}) => {
   const category = route.params;
   const {height} = useWindowDimensions();
   const {current} = useCardAnimation();
 
-  const [categoryName, setCategoryName] = useState("");
-
-  React.useEffect(() => {
-    setCategoryName(category.Name);
-  }, []);
+  const [value, setValue] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
   const styles = StyleSheet.create({
-  viewAnimated: {
-    width: '100%',
-  },
-  viewContainer: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#E5E5E5',
-    borderRadius: 20,
-  },
-});
+    viewAnimated: {
+      width: '100%',
+    },
+    viewContainer: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: '#E5E5E5',
+      borderRadius: 20,
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+  });
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',}}>
+      {/*<Modal*/}
+      {/*  animationType='slide'*/}
+      {/*  transparent={true}*/}
+      {/*  visible={modalVisible}>*/}
+      {/*  <View style={styles.centeredView}>*/}
+      {/*    <View style={styles.modalView}>*/}
+      {/*      <Text h4={true}>Заметки</Text>*/}
+      {/*      <Input/>*/}
+      {/*      <View style={{flexDirection: 'row'}}>*/}
+      {/*        <Button type='clear' onPress={() => setModalVisible(false)}>Отмена</Button>*/}
+      {/*        <Button type='clear'>ОК</Button>*/}
+      {/*      </View>*/}
+      {/*    </View>*/}
+      {/*  </View>*/}
+      {/*</Modal>*/}
       <Pressable style={[StyleSheet.absoluteFill, {backgroundColor: "rgba(0,0,0,0.7)"}]}
                  onPress={navigation.goBack}/>
       <Animated.View
@@ -37,7 +73,7 @@ const AddValueToCategoryScreen = ({navigation, route}) => {
               {
                 translateY: current.progress.interpolate({
                   inputRange: [0, 1],
-                  outputRange: [height, height * 0.3],
+                  outputRange: [height, height * 0.45],
                   extrapolate: 'clamp',
                 }),
               },
@@ -46,7 +82,25 @@ const AddValueToCategoryScreen = ({navigation, route}) => {
           styles.viewAnimated
         ]}>
         <View style={styles.viewContainer}>
-          <Text>{categoryName}</Text>
+          <View style={{borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth}}>
+            <Text h1={true} style={{textAlign: 'center'}}>{value}</Text>
+          </View>
+          <View style={{borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth}}>
+            <Button type="clear" titleStyle={{color: 'grey'}} onPress={() => setModalVisible(true)}>Заметки...</Button>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style={{flex: 5}}>
+              <VirtualKeyboard decimal={true} onPress={val => setValue(val)}/>
+            </View>
+            <View style={{flex: 1}}>
+              <View style={{flex: 1}}></View>
+              <View style={{flex: 1}}>
+                <Button buttonStyle={{height: '100%'}}>
+                  <Icon name='check'/>
+                </Button>
+              </View>
+            </View>
+          </View>
         </View>
       </Animated.View>
     </View>
