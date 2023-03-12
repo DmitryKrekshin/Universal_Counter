98fcbc85d5fe35a2cc6c19657df7cafa68c019a2
Dmitriy Krekshin <dima.krejshin@gmail.com>
2023-03-12 15:14:09 +0300

    add todo comment

--- MobileApp/src/Components/Navigators/TabNavigator.js
+++ MobileApp/src/Components/Navigators/TabNavigator.js
@@ -5,10 +5,10 @@ import HistoryListScreen from "../Screens/HistoryListScreen";
 const BottomTabNavigator = createBottomTabNavigator();
 
 const TabNavigator = () => {
-  return (
+  return ( //todo не забыть убрать id 11
     <BottomTabNavigator.Navigator>
       <BottomTabNavigator.Screen name='Categories' component={CategoriesStackNavigator} options={{headerShown: false}}/>
-      <BottomTabNavigator.Screen name='ForTesting' component={HistoryListScreen} initialParams={{categoryId: 10}}/>
+      <BottomTabNavigator.Screen name='ForTesting' component={HistoryListScreen} initialParams={{categoryId: 11}}/>
     </BottomTabNavigator.Navigator>
   );
 }
